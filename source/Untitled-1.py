a10e884896b05b2a6a5296e9c19178291cc43755
yuchuan0528 <yuchuan0528@163.com>
2022-05-05 15:40:57 +0800

    update cookbook

---
+++ source/Untitled-1.py
@@ -0,0 +1,8 @@
+
+{
+    "python.pythonPath": "./venv/bin/python3.7",
+    "restructuredtext.confPath": "${workspaceFolder}",
+    "editor.detectIndentation": false,
+    "editor.insertSpaces": true,
+    "editor.tabSize": 3,
+}
